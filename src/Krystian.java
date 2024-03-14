a0c50a32a36a7ad25011a50114e5edafd6fa035c
Przemek B <pprzemek89@gmail.com>
2024-03-14 13:22:41 +0100

    Roch i Krystian

---
+++ src/Krystian.java
@@ -0,0 +1,10 @@
+public class Krystian implements Uczen{
+    @Override
+    public void uczSie() {
+        System.out.println("Uczę się bo Monika mnie zmusza");
+    }
+
+    public void ogladajKiepskich(){
+        System.out.println("Ogladam Kiepskich w morde jeza");
+    }
+}
---
+++ src/Roch.java
@@ -0,0 +1,12 @@
+public class Roch implements Uczen{
+
+    @Override
+    public void uczSie() {
+        System.out.println("Bleeee duzo mowie i sie ucze");
+    }
+    
+    public void grajWSzachy(){
+        System.out.println("Właśnie pokonałem pana Ernesta :)");
+    }
+}
+
